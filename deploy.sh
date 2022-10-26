b04683858582cfcd525b5563b1fdac82d3e3eee1
eric <howcrazys@gmail.com>
2022-10-26 15:04:52 +0800

    修正 sh ghpage 位置

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/kerickkkkk/homeless.git master:gh-pages
+git push -f https://github.com/kerickkkkk/d3-practice.git master:gh-pages
 
 cd -
 Footer
